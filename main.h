074ff61a0c04ccaef5e0b941b8cd21991e7081b1
tamerkat <mahad10taha@gmail.com>
2023-12-03 13:56:29 +0200

    add comments in main.h

--- main.h
+++ main.h
@@ -9,12 +9,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
+/*buffer size*/
 #define BUFFER_SIZE 1024
 #define FLUSH_BUFFER_SIZE -1
 
 
-
+/*prototypes*/
 int s_strlen(int *s);
 int s_strcmp(const char *str1, const char *str2);
 char *s_start_w(const char *str1, const char *str2);
