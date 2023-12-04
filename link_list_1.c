11a9621b046ab49e97d3a212f45fcf39a8eed90b
tamerkat <mahad10taha@gmail.com>
2023-12-04 22:09:15 +0200

    add linked list function in 2 file

---
+++ link_list_1.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+/**
+ * print_node_list - Entry point
+ * @h: double pointer
+ * Return: always true
+*/
+size_t print_node_list(const node_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		s_put(convert_num(h->n, 10, 0));
+		s_putchar(':');
+		s_putchar(' ');
+		s_put(h->s ? h->s : "(nil)");
+		s_put('\n');
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+
+/**
+ * printf_node_list -Entry point
+ * @h: double pointer
+ * Return: always true
+*/
+size_t printf_node_list(const node_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		s_put(h->s ? h->s : "(nil)");
+		s_put("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * del_node_index -Entry point
+ * @head: double pointer
+ * @index: double pointer
+ * Return: always true
+*/
+int del_node_index(node_t **head, unsigned int index)
+{
+	node_t *node, *b_node;
+
+	unsigned int i;
+
+	if (!head || !*head)
+		return (0);
+	if (!index)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node->s);
+		free(node);
+		return (1);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == index)
+		{
+			b_node->next = node->next;
+			free(node->s);
+			free(node);
+			return (1);
+		}
+		i++;
+		b_node = node;
+		node = node->next;
+	}
+	return (0);
+}
+
+
+/**
+ * add_node_head -Entry point
+ * @head: double pointer
+ * @str: pointer
+ * @num: number
+ * Return: always true
+*/
+node_t *add_node_head(node_t **head, char *str, int num)
+{
+	node_t *new_node;
+
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(node_t));
+	if (!new_node)
+		return (NULL);
+	s_memset((void *)new_node, 0, sizeof(node_t));
+	new_node->n = num;
+	if (str)
+	{
+		new_node->s = s_strdup(str);
+		if (!new_node->s)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
---
+++ link_list_2.c
@@ -0,0 +1,118 @@
+#include "shell.h"
+
+/**
+ * add_node_end -Entry point
+ * @head: double pointer
+ * @str: pointer
+ * @num: number
+ * Return: always true
+*/
+node_t *add_node_end(node_t **head, char *str, int num)
+{
+	node_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+	node = *head;
+	new_node = malloc(sizeof(node_t));
+	if (!new_node)
+		return (NULL);
+	s_memset((void *)new_node, 0, sizeof(node_t));
+	new_node->n = num;
+	if (str)
+	{
+		new_node->s = s_strdup(str);
+		if (!new_node->s)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+
+/**
+ * free_node - Entry point
+ * @head: double pointer
+ * Return: always true
+*/
+void free_node(node_t **head)
+{
+	node_t *node, *next_node, *hhead;
+
+	if (!head || !*head)
+		return;
+	hhead = *head;
+	node = hhead;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->s);
+		free(node);
+		node = next_node;
+	}
+	*head = NULL;
+}
+
+/**
+ * s_list_len - Entry point
+ * @h: double pointer
+ * Return: always true
+*/
+size_t s_list_len(node_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * s_list_to_str - Entry point
+ * @head: double pointer
+ * Return: always true
+*/
+char **s_list_to_str(node_t *head)
+{
+	node_t *node = head;
+	size_t i = s_list_len(head);
+	size_t j;
+	char **s;
+	char *str;
+
+	if (!head || !i)
+	return (NULL);
+	s = malloc(sizeof(char *) * (i + 1));
+	if (!s)
+	return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+	str = malloc(s_strlen(node->s) + 1);
+	if (!str)
+	{
+	for (j = 0; j < i; j++)
+	free(s[j]);
+	free(s);
+	return (NULL);
+	}
+
+	str = s_strcpy(str, node->s);
+	s[i] = str;
+	}
+	s[i] = NULL;
+	return (s);
+}
